d666a22c1e53e8de1e4d91de5b71e526a834c839
Pratik Shahi <shahipr0@gmail.com>
2021-03-23 16:21:36 +0530

    used express chainable route handlers

--- app.js
+++ app.js
@@ -28,29 +28,39 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundItem) {
-    if (!err) {
-      res.send(foundItem);
-    } else {
-      res.send(err);
-    }
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundItem) {
+      if (!err) {
+        res.send(foundItem);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        console.log("SUcessfully Saved New Article");
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("sucessfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
-
-app.post("/articles", function (req, res) {
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  newArticle.save(function (err) {
-    if (!err) {
-      console.log("SUcessfully Saved New Article");
-    } else {
-      console.log(err);
-    }
-  });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
